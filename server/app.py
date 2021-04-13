cf0efd50a71cadcf3fc049cf37e5a89e0d4b559e
hssaka7 <hssaka7@gmail.com>
2021-04-12 20:16:18 -0500

    Added put mehtod for updating single mood

--- server/app.py
+++ server/app.py
@@ -47,6 +47,29 @@ def all_moods():
     
     return jsonify(response_object)
 
+@app.route('/moods/<mood_id>', methods = ['PUT'])
+def single_mood (mood_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_mood(mood_id)
+        MOODS.append({
+            'id': uuid.uuid4().hex,
+            'name': post_data.get('name'),
+            'timestamp': post_data.get('timestamp'),
+        })
+        response_object['message'] = 'Mood Added'
+    else:
+        response_object['moods'] = MOODS
+    return jsonify(response_object)
+
+def remove_mood(mood_id):
+    for mood in MOODS:
+        if moood['id'] == mood_id:
+            MOODS.remove(mood)
+            return True
+        return False
+
 
 if __name__ == '__main__':
     app.run()
